e0eff4a3632e4dd3e8139b038a59289827f857f3
Jimmy Zheng <jime82075@gmail.com>
2024-02-12 23:35:15 -0500

    css works and page routing works.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Uses https://github.com/nolanbconaway/underground/tree/main
-1) Create and .env file 
-2) Add MTA_API_KEY="Your mta api key"
+
+1. Create and .env file
+2. Add MTA_API_KEY="Your mta api key"
--- src/main.py
+++ src/main.py
@@ -8,6 +8,14 @@ def home():
     return render_template("index.html")
 
 
+# Define a route that takes a parameter for the train line
+@app.route("/train/<train_line>")
+def train_schedule(train_line):
+    # Here you can dynamically generate the schedule for the selected train line
+    # For demonstration purposes, let's just return a simple message
+    return render_template("line.html")
+
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
