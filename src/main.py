266d230a017f32c02074fdbf7cab5f0a1e46ab2c
Jimmy Zheng <jime82075@gmail.com>
2024-02-12 23:35:34 -0500

    test

--- src/main.py
+++ src/main.py
@@ -8,11 +8,8 @@ def home():
     return render_template("index.html")
 
 
-# Define a route that takes a parameter for the train line
 @app.route("/train/<train_line>")
 def train_schedule(train_line):
-    # Here you can dynamically generate the schedule for the selected train line
-    # For demonstration purposes, let's just return a simple message
     return render_template("line.html")
 
 
